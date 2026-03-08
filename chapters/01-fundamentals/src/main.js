b2ceee39032c160377e8cfc010671409a1133f63
Ryan Haynes <ryan.haynes@gmail.com>
2026-03-07 20:54:27 -0700

    Fixed naming error

--- chapters/01-fundamentals/src/main.js
+++ chapters/01-fundamentals/src/main.js
@@ -2,4 +2,4 @@ const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
 // Delete this line to get started and replace it with your tutorial code!
-helloWorld2();
+helloWorld();
